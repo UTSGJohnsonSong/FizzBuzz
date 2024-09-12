30e61d05e47e0e2a77439e7720f6786ce0b5c230
UTSGJohnsonSong <zekun.song@mail.utoronto.ca>
2024-09-12 18:49:19 -0400

    Complete Reduce.java part

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,4 +1,18 @@
-package PACKAGE_NAME;
-
 public class Reduce {
+    public static int main(int n) {
+        int steps = 0;
+        while (n > 0) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n--;
+            }
+            steps++;
+        }
+        return steps;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(main(100));
+    }
 }
